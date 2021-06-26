54325d9ee1c39ea2ba00472d8ac2bb3aaa02954d
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-06-26 15:55:46 +0700

    delete:old-auth

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -21,62 +21,6 @@ class AdminController extends Controller
         $user = User::all();
         return view('accounts.index', ['users' => $user]);
     }
-    
-    public function loginPage() {
-        return view('auth.login');
-    }
-    public function registerPage() {
-        return view('auth.register');
-    }
-
-    public function login(AdminLoginRequest $request) 
-    {
-        $username = $request->username;
-        $password = $request->password;
-
-        if (filter_var($username, FILTER_VALIDATE_EMAIL)) {
-            Auth::attempt(['email' => $username, 'password' => $password]);
-        } else {
-            Auth::attempt(['username' => $username, 'password' => $password]);
-        }
-
-        if(Auth::check()) {
-            return redirect()->route('dashboard');
-        } else {
-            return redirect()->back()->with('message', 'login gagal');
-        }
-    }
-
-    public function register(AdminRegisterRequest $request)
-    {
-        DB::beginTransaction();
-        try {
-            $user = User::create([
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-                'role' => 'admin',
-            ]);
-            Admin::create([
-                'id_user' => $user->id,
-                'nama' => $request->nama
-            ]);
-        } catch (Exception $e) {
-            DB::rollback();
-            return redirect()->back()->with('error, register gagal');
-        }
-        DB::commit();
-        
-        try{
-            Auth::attempt(['username' => $request->username, 'password' => $request->password]);
-            if(Auth::check()) 
-                return view('dashboard');
-            else
-                return redirect()->back()->with('message', 'silakan login secara manual');
-        } catch (Exception $e) {
-            return redirect()->back()->with('message', 'silakan login secara manual');
-        }
-    }
 
     public function logout(Request $request) 
     {
